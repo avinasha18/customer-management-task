089e7298ad784f18a75b19395c97d5da13370752
Tejassri Avinasha <136706216+avinasha18@users.noreply.github.com>
2024-09-05 17:15:34 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -9,8 +9,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+const corsOptions = {
+    origin: 'https://customer-management-task-1.onrender.com', // Your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  };
+  
+  app.use(cors(corsOptions));
+  app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
