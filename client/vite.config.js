5d2847a6dd500c17a20f7983f04ae70bbbc7c54a
Tejassri Avinasha <136706216+avinasha18@users.noreply.github.com>
2024-09-05 17:50:34 +0530

    build

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,19 +3,22 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  darkMode: 'class',
-  postcss: {
-    plugins: [tailwindcss()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
   },
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 5173,
   },
+  build: {
+    sourcemap: mode === 'production' ? false : true,  // Disable source maps in production
+  },
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT || 4173,
   },
-})
-
+}))
